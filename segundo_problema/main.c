9bbc6e873b5e6fcffd031ac77941216390490620
jfrbr <juan.franca@gmail.com>
2014-01-22 18:46:16 -0200

    Problema no segundo problema

--- segundo_problema/main.c
+++ segundo_problema/main.c
@@ -3,9 +3,37 @@
 #include <stdlib.h>
 
 #define MAX 150
+#define INF 2000000000
 
+int n;
 int g[MAX][MAX];
 int parent[MAX];
+int queue[MAX],ind;
+
+// queue implementation
+
+void push(int elem){
+	printf("Index = %d\n",ind);
+	queue[ind++] = elem;
+}
+
+int front(){
+	return ind > 0 ? queue[0] : -999 ;
+}
+
+void pop(){
+	int i;
+	for (i=1; i <= ind; ++i){
+		queue[i-1] = queue[i];
+	}
+	ind--;
+}
+
+int empty(){
+	return ind == 0 ? 1 : 0;
+}
+
+// max flow algorithm
 
 int f,s,t;
 
@@ -21,6 +49,7 @@ void augment_path( int v, int min_edge ){
 	} else if ( parent[v] != -1 ){
 		
 		augment_path(parent[v], MIN(min_edge, g[parent[v]][v]));
+
 		g[ parent[v] ][ v ] -= f;
 		g[ v ][parent[v] ] += f;
 	}
@@ -28,21 +57,50 @@ void augment_path( int v, int min_edge ){
 
 int max_flow(int src, int dest){
 
+	int flow=0,i,dist[MAX];
+	memset(dist,0,sizeof(dist));
 
+	while (1){
 
+		f = 0;
 
+		push(s);
+		dist[s] = 0;
 
-	
+		while ( !empty() ){
+
+			int u = front();
+			pop();
+			if (u == t) break;
+
+			for (i=1;i<=n;++i){
+				if ( g[u][i] > 0 && !dist[i] ){
+					dist[i] = dist[u] + 1 ;
+					push(i);
+					parent[i] = u;
+				}
+			}	
+		
+		}
+
+		augment_path(t,INF);
+
+		if (f == 0) break;
+		flow += f;
+	}
+
+	return flow;
 }
 
 int main(){
 
-	int i,n,c,a,b,w;
+	int i,c,a,b,w,tests=1;
 
-	while ( scanf("%d\n") == 1 && n ){
+	while ( scanf("%d\n",&n) == 1 && n ){
 
 		memset(g,0,sizeof(g));
 		memset(parent,-1,sizeof(parent));
+		memset(queue,0,sizeof(queue));
 
 		f = s = t = 0;
 
@@ -51,18 +109,15 @@ int main(){
 
 		for (i=0;i<c;++i){
 			scanf("%d %d %d\n",&a,&b,&w);
-			g[a][b] = 1;
-			g[b][a] = 1;
+			g[a][b] = w;
+			g[b][a] = w;
+//			printf("%d %d %d\n",a,b,w);
 		}
 
 		int res = max_flow(s,t);
 
-		// max flow
-		// print solucao
-
+		printf("Rede %d\nA largura de banda é %d.\n",tests++,res);
 	
 	}
-
-
 	return 0;
 }
